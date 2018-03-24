e08d8e6b74bbfe3be8caa9289564a7cff550c429
bilalkhanit <qau.bilal@gmail.com>
2018-03-24 11:02:40 +0500

    Git initial commit

---
+++ MY_BUDDY/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MY_BUDDY
+//
+//  Created by Bilal on 24/03/2018.
+//  Copyright © 2018 Bilal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
